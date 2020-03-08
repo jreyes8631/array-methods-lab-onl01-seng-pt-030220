acce4b009b1f8391cdaab382ca4abb3b4857912e
Jose E Reyes <jreyes8631@gmail.com>
2020-03-08 22:03:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,18 +7,22 @@ def using_sort(array)
 
 end
 
-def using_reverse(array)
+def using_reverse(array
+  array.reverse
 
 end
 
 def using_first(array)
+  array.first
 
 end
 
 def using_last(array)
+  array.last
 
 end
 
 def using_size(array)
+  array.size
 
 end
